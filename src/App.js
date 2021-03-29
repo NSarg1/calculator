bdb11dcd74c3adda586601f6af114de80557923a
Narek <NSarg7@gmail.com>
2021-03-29 20:55:26 +0400

    Created calculator some functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./app.module.scss";
+
+const INITIAL_RESULT_STATE = "0";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [result, setResult] = useState(INITIAL_RESULT_STATE);
+
+    const handleNumClick = (num) => {
+        if (result === "0") {
+            setResult(num);
+        } else {
+            setResult(result + num);
+        }
+    };
+
+    const handleReset = () => setResult(INITIAL_RESULT_STATE);
+
+    const handleDelete = () => {
+      const newString =  result.slice(0, -1);
+      if(newString === "") setResult(INITIAL_RESULT_STATE);
+      else setResult(newString);
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.calculator}>
+                <div className={styles.result}>{result}</div>
+                <div className={styles.buttons}>
+                    <ul className={styles.row}>
+                        <li className={styles.column} onClick={handleReset}>C</li>
+                        <li className={styles.column} onClick={handleDelete}>Del</li>
+                        <li className={styles.column}>%</li>
+                        <li className={styles.column}>/</li>
+                    </ul>
+                    <ul className={styles.row}>
+                        <li className={styles.column} onClick={() => handleNumClick("7")}>
+                            7
+                        </li>
+                        <li className={styles.column} onClick={() => handleNumClick("8")}>
+                            8
+                        </li>
+                        <li className={styles.column} onClick={() => handleNumClick("9")}>
+                            9
+                        </li>
+                        <li className={styles.column}>*</li>
+                    </ul>
+                    <ul className={styles.row}>
+                        <li className={styles.column} onClick={() => handleNumClick("4")}>
+                            4
+                        </li>
+                        <li className={styles.column} onClick={() => handleNumClick("5")}>
+                            5
+                        </li>
+                        <li className={styles.column} onClick={() => handleNumClick("6")}>
+                            6
+                        </li>
+                        <li className={styles.column}>-</li>
+                    </ul>
+                    <ul className={styles.row}>
+                        <li className={styles.column} onClick={() => handleNumClick("1")}>
+                            1
+                        </li>
+                        <li className={styles.column} onClick={() => handleNumClick("2")}>
+                            2
+                        </li>
+                        <li className={styles.column} onClick={() => handleNumClick("3")}>
+                            3
+                        </li>
+                        <li className={styles.column}>+</li>
+                    </ul>
+                    <ul className={styles.row}>
+                        <li
+                            className={styles.column}
+                            style={{ flex: "2 0" }}
+                            onClick={() => handleNumClick("0")}>
+                            0
+                        </li>
+                        <li className={styles.column}>.</li>
+                        <li className={styles.column}>=</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
